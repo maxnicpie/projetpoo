c8da36480f242fa8db552400b0d90cfac001c497
unknown <Nicolas@.(none)>
2014-02-03 10:36:01 +0100

    ajout classe etoile

--- ProjetPOO/src/Etoile.java
+++ ProjetPOO/src/Etoile.java
@@ -1,5 +1,18 @@
 
 public enum Etoile {
 	
-
+	UNE_ETOILE("Une étoile", 1),
+	DEUX_ETOILES("Deux étoiles", 2),
+	TROIS_ETOILES("Trois étoiles", 3),
+	QUATRE_ETOILES("Quatre étoiles", 4),
+	CINQ_ETOILES("Cinq étoiles", 5);
+	
+	private String intitule;
+	private int etoiles;
+	
+	Etoile(String intitule, int etoiles){
+		this.intitule=intitule;
+		this.etoiles=etoiles;
+	}
+	
 }
