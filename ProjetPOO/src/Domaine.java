5921f489c152516824c982a03612d10371c187fe
Pierrick <Pierrick@172.21.71.200>
2014-02-05 11:09:35 +0100

    modif afficherListeDomaines

--- ProjetPOO/src/Domaine.java
+++ ProjetPOO/src/Domaine.java
@@ -1,3 +1,5 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import com.mysql.jdbc.Statement;
 
@@ -5,33 +7,37 @@ public class Domaine {
 
 	private String nom;
 	private Statement st;
-	
+	private ResultSet rs;
+
 	public Domaine(String nom) {
 		this.nom = nom;
 	}
-	
-	public Domaine() {}
-	
+
+	public Domaine() {
+	}
+
 	public String getNom() {
 		return nom;
 	}
- 
+
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
-	
+
 	public void creerDomaine() {
-		String CREER_DOMAINE = "INSERT INTO DOMAINE VALUES (null,\""+nom+"\")";
+		String CREER_DOMAINE = "INSERT INTO DOMAINE VALUES (null,\"" + nom
+				+ "\")";
 		try {
-			st.executeQuery(CREER_DOMAINE);
+			st.executeUpdate(CREER_DOMAINE);
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}	
+		}
 	}
-	
+
 	public void supprimerDomaine() {
-		String SUPPRIMER_DOMAINE = "DELETE FROM DOMAINE WHERE nomDomaine=\""+nom+"\"";
+		String SUPPRIMER_DOMAINE = "DELETE FROM DOMAINE WHERE nomDomaine=\""
+				+ nom + "\"";
 		try {
 			st.executeQuery(SUPPRIMER_DOMAINE);
 		} catch (SQLException e) {
@@ -39,14 +45,27 @@ public class Domaine {
 			e.printStackTrace();
 		}
 	}
-	
-	public void afficherListeDomaines() {
+
+	public String[] afficherListeDomaines() {
 		String AFFICHER_LISTE_DOMAINES = "SELECT nomDomaine FROM DOMAINE";
+		int nbLignes = 0;
+		int i = 1;
+		String[] listeDomaines = new String[nbLignes + 1];
+		listeDomaines[0] = "";
+
 		try {
-			st.executeQuery(AFFICHER_LISTE_DOMAINES);
+			rs = st.executeQuery(AFFICHER_LISTE_DOMAINES);
+			rs.last();
+			nbLignes = rs.getRow();
+			rs.first();
+			while (i != nbLignes + 1) {
+				listeDomaines[i] = rs.getString(1);
+				i++;
+				rs.next();
+			}
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		return listeDomaines;
 	}
 }
